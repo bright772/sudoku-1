7194ba42dec95365c9a43ae52ddc3f78706d94c2
aaramour <aaramour@byui.edu>
2022-03-22 23:22:21 -0400

    here we go

---
+++ list_handler.py
@@ -0,0 +1,12 @@
+
+class List_Handler:
+
+    def __init__(self):
+        self.row_check = True
+        self.column_check = True
+        self.square_check = True
+
+    #  def check_row(self,grid[],):
+        # for row in grid:
+        
+            
