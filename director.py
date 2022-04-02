6f51a5a4ad1a6d6c24bcd40ffca49f14da7d8ea5
Emily <cas20037@byui.edu>
2022-04-01 20:12:46 -0600

    read me

--- director.py
+++ director.py
@@ -1,22 +1,22 @@
 from action import Action
 from sudoku import Sudoku
 from sudoku_handling import Handling
-from text import Prompts
 import constants
 
 
 class Director:
+    """Plays the game"""
 
     def __init__(self):
         self.is_playing = True
         self.action = Action()
         self.sudoku = Sudoku()
         self.handling = Handling()
-        self.text = Prompts()
-        constants.GAME_GRID = self.sudoku.game_grid_maker(constants.ANSWER_GRID)
-        constants.GAME_GRID_BLANK = constants.GAME_GRID
+        constants.GAME_GRID = self.sudoku.game_grid_maker(
+            constants.ANSWER_GRID)
 
     def start_game(self):
+        """Starts the game"""
         print("Welcome to the Sudoku Game!")
 
         while self.is_playing:
@@ -28,8 +28,6 @@ class Director:
             self.action.todo()
             if self.action.instruction == 3:
                 break
-            # print(f"constants.ROW: {constants.ROW}") 
-            self.handling.num_inserter() 
-            self.handling.win_checker()
+            self.handling.print_board(constants.GAME_GRID)
 
         self.is_playing = False
--- sudoku_handling.py
+++ sudoku_handling.py
@@ -1,6 +1,8 @@
 from sudoku import Sudoku
 from text import Prompts
 import constants
+from main import GameBoard
+
 
 class Handling():
     """Handles inserting and removing a number"""
@@ -9,21 +11,6 @@ class Handling():
         self.sudoku = Sudoku()
         self.text = Prompts()
         self.game_grid = constants.GAME_GRID
-        
-    # def game_grid_maker(self, grid, difficulty_variable=4):
-    #     if self.text.column == self.sudoku.cols and self.text.row == self.sudoku.rows:
-    #         game_grid = []
-    #         for row in grid:
-    #             self.number = self.text.number
-    #             row.insert(" ", self.number)
-    #         game_grid.append(row)
-    #         return super().game_grid_maker(grid, difficulty_variable)
-
-    # def print_board(self, grid, rows=..., cols=...):
-    #     return super().print_board(grid, rows, cols)
-
-# NOTE: Im trying to rewrite the code from Sudoku to insert the users's input number
-
 
     def num_inserter(self):
         # print(f"constants.ROW_NUMBERS: {constants.ROW_NUMBERS}")
@@ -35,16 +22,14 @@ class Handling():
         # print(f"column_index: {column_index}")
         if constants.GAME_GRID[row_index][column_index] == " ":
             constants.GAME_GRID[row_index].pop(column_index)
-            constants.GAME_GRID[row_index].insert(column_index, constants.NUMBER)
+            constants.GAME_GRID[row_index].insert(
+                column_index, constants.NUMBER)
         else:
             print("You can only insert a number into a blank space.")
 
     def win_checker(self):
-        for row in constants.GAME_GRID: 
-            if " " in row: #Checks each row in the game grid for a space. If it finds one, this function ends.
-                return #the game will continue until the grid is full.
-        
-        #TODO: make the win condition. My idea was to compare if GAME_GRID == ANSWER_GRID is a win, anything else resets the game by making GAME_GRID = GAME_GRID_BLANK
-        
-    
-        
+        for row in constants.GAME_GRID:
+            if " " in row:  # Checks each row in the game grid for a space. If it finds one, this function ends.
+                return  # the game will continue until the grid is full.
+
+        # TODO: make the win condition. My idea was to compare if GAME_GRID == ANSWER_GRID is a win, anything else resets the game by making GAME_GRID = GAME_GRID_BLANK
