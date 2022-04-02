1bf19c44e0c31d2f47c56b310e1e79e0e0563fe2
aaramour <aaramour@byui.edu>
2022-04-02 01:02:27 -0400

    this is as much as I can do

--- sudoku_handling.py
+++ sudoku_handling.py
@@ -13,18 +13,12 @@ class Handling():
 
     def num_inserter(self):
         """Inserts a number according to user's input"""
-        # print(f"constants.ROW_NUMBERS: {constants.ROW_NUMBERS}")
-        # print(f"constants.ROW: {constants.ROW}") #TODO: This only prints 0. self.text.row is the issue.
         row_index = constants.ROW_NUMBERS.index(constants.ROW)
-        # print(f"constants.COLUMN_LETTERS: {constants.COLUMN_LETTERS}")
-        # print(f"row_index: {row_index}")
         column_index = constants.COLUMN_LETTERS.index(constants.COLUMN)
-        # print(f"column_index: {column_index}")
-        if constants.GAME_GRID[row_index][column_index] == " ":
+        if constants.GAME_GRID[row_index][column_index] == " ": #if the list has a space, replace it with the number
             constants.GAME_GRID[row_index].pop(column_index)
             constants.GAME_GRID[row_index].insert(
                 column_index, constants.NUMBER)
-            # print(constants.NUMBER)
         else:
             print("You can only insert a number into a blank space.")
 
@@ -38,12 +32,28 @@ class Handling():
             constants.GAME_GRID[row_index].insert(
                 column_index, constants.NUMBER)
 
-    def win_checker(self):
-        """"Checks when the user wins"""
+    def win_checker(self, is_playing):
+        """Checks when the user wins"""
         for row in constants.GAME_GRID:
             if " " in row:  # Checks each row in the game grid for a space. If it finds one, this function ends.
-                return  # the game will continue until the grid is full.
+                return is_playing# the game will continue until the grid is full.
 
         # compares actual grid with the correct answer grid
-        if constants.GAME_GRID == constants.ANSWER_GRID:
-            print("YAY! You Won!")
+        winning = True
+        for i in range(len(constants.COLUMN_LETTERS)):
+            for j in range(len(constants.COLUMN_LETTERS)):
+                if constants.GAME_GRID[i][j] == constants.ANSWER_GRID[i][j] and winning == True:
+                    winning = True
+                else:
+                    winning = False
+                
+        
+        if winning:
+            print("Yes! You won the game!")
+            is_playing = False
+            return is_playing
+        else:
+            print("You lost the game.")
+            is_playing = False
+            return is_playing
+
